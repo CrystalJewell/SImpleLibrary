3040aef0e800673647a33475fd15959096e6b3da
Crystal Adkins <crystaladkins09@gmail.com>
2017-07-11 08:57:20 -0400

    Added program logic comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        // 1. Create Class for Books.
+        // 2. Test that Book class works as intended.
+        // 3. Create Class for Library.
+        // 4. Create methods for adding, removing, checking out, returning and listing books in library, currently available or checked out.
+        // 5. Test that Library class is working as intended.
+        // 6. Allow user input for adding, removing, checking out and returning book.
+        // 7. Allow user to see what books the library has, what is available or what is checked out.
 
     }
 }
