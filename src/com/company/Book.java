b8063d0d3255cdba604dbafb9f8a77814cba4055
Crystal Adkins <crystaladkins09@gmail.com>
2017-07-11 09:43:44 -0400

    Created Library Class, Commented out test in Book class

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -29,14 +29,14 @@ public class Book {
         return isBorrowed;
     }
 
-    //Used only in this class to test that the code we created is working the way we intended
-    public static void main(String[] arguments) {
-        Book example = new Book("Hitchhikers Guide To The Galaxy");
-        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
-        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-        example.setBorrowed(example.isBorrowed);
-        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
-        example.setBorrowed(example.isBorrowed);
-        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-    }
+//    //Used only in this class to test that the code we created is working the way we intended
+//    public static void main(String[] arguments) {
+//        Book example = new Book("Hitchhikers Guide To The Galaxy");
+//        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+//        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+//        example.setBorrowed(example.isBorrowed);
+//        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+//        example.setBorrowed(example.isBorrowed);
+//        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+//    }
 }
---
+++ src/com/company/Library.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by crystaladkins on 7/11/17.
+ */
+public class Library {
+
+    
+}
