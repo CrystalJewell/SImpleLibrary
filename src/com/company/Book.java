2f43ca5fb846a71703dd736ffbd853b12094af9b
Crystal Adkins <crystaladkins09@gmail.com>
2017-07-11 09:13:02 -0400

    Created Book class, needed variables and constructor for new book.

---
+++ src/com/company/Book.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by crystaladkins on 7/11/17.
+ */
+public class Book {
+
+    private String title;
+    private boolean isBorrowed;
+
+    //Constructor to create a new book
+    public Book (String bookTitle) {
+
+        title = bookTitle;
+        isBorrowed = false;
+    }
+}
